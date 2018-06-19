535fe05e8745425fe15513c8d4244138c7c9e778
Zilvinas Ringele <zilvinas.ringele@gmail.com>
2018-06-19 12:14:42 +0300

    Updating

--- Client.php
+++ Client.php
@@ -5,7 +5,10 @@ class Client
 * Class for Client inserting, updating and editing.
 */ 
 {
-    public $conn = '';
+    public $dbserver = 'localhost';
+    public $dbname = 'root';
+    public $dbuser = '';
+    public $dbpass = 'client_management_system';
 
     public $id;
     public $firstname;
@@ -15,8 +18,44 @@ class Client
     public $phonenumber2;
     public $comment;
 
-    public function __construct($action)
+    public function __construct($data)
     {
-        switch($action['action'])
+        foreach ($data as $column => $value)
+        {
+            switch ($column){
+                case 'id':
+                    $this->id = $value;
+                    break;
+                case 'firstname':
+                    $this->firstname = $value;
+                    break;
+                case 'lastname':
+                    $this->lastname = $value;
+                    break;
+                case 'email':
+                    $this->email = $value;
+                    break;
+                case 'phonenumber1':
+                    $this->phonenumber1 = $value;
+                    break;
+                case 'phonenumber2':
+                    $this->phonenumber2 = $value;
+                    break;
+                case 'comment':
+                    $this->comment = $value;
+                    break;
+            }
+        }
+    }
+    public function insert()
+    {
+        $mysqli = new mysqli($dbserver, $dbuser, $dbpass, $dbname);
+        $query = $mysqli->real_escape_string($this->firstname);
+        var_dump($query);
+        return $query;
+    }
+    public function delete()
+    {
+        return 'succesfully deleted user '. $this->firstname;
     }
 }
--- admin_panel.php
+++ admin_panel.php
@@ -2,6 +2,24 @@
 parse_str(implode('&', array_slice($argv, 1)), $_GET);
 
 require_once 'Client.php';
-if ($_GET['action'] != 'import'){
-    new Client($_GET);
+if(!isset($_GET['action'])) 
+    echo "Please define action. action must be import, insert, update or delete";
+switch ($_GET['action'])
+{
+    case 'insert':
+        $client = new Client($_GET);
+        echo $client->insert();
+        break;
+    case 'update':
+        $client = new Client($_GET);
+        echo $client->update();
+        break;
+    case 'import':
+        // $client = new Client($_GET);
+        // echo $client->insert();
+        break;
+    case 'delete':
+        $client = new Client($_GET);
+        echo $client->delete();
+        break;
 }
