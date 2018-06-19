5220197373caf3aaa0bd0243bfa2bc46d1befb67
Zilvinas Ringele <zilvinas.ringele@gmail.com>
2018-06-19 16:01:27 +0300

    Update

--- Client.php
+++ Client.php
@@ -21,9 +21,11 @@ class Client
     public function __construct($data)
     {
         $mysqli = new mysqli($this->dbserver, $this->dbuser, $this->dbpass, $this->dbname);
+        $validation = new Validation;
         foreach ($data as $column => $value)
         {
-            switch ($column){
+            switch ($column)
+            {
                 case 'id':
                     $this->id = $mysqli->real_escape_string($value);
                     break;
@@ -34,13 +36,25 @@ class Client
                     $this->lastname = $mysqli->real_escape_string($value);
                     break;
                 case 'email':
-                    $this->email = $mysqli->real_escape_string($value);
+                    if ($validation->validateEmail($value)){
+                        $this->email = $mysqli->real_escape_string($value);
+                        break;
+                    }
+                    echo 'email is not valid';
                     break;
                 case 'phonenumber1':
-                    $this->phonenumber1 = $mysqli->real_escape_string($value);
+                    if ($validation->validateEmail($value)){
+                        $this->phonenumber1 = $mysqli->real_escape_string($value);
+                        break;
+                    }
+                    echo 'Phone number 1 is not valid';
                     break;
                 case 'phonenumber2':
-                    $this->phonenumber2 = $mysqli->real_escape_string($value);
+                if ($validation->validateEmail($value)){
+                        $this->phonenumber2 = $mysqli->real_escape_string($value);
+                        break;
+                    }
+                    echo 'Phone number 2 is not valid';
                     break;
                 case 'comment':
                     $this->comment = $mysqli->real_escape_string($value);
@@ -48,6 +62,7 @@ class Client
             }
         }
     }
+
     public function insert()
     {
         $mysqli = new mysqli($this->dbserver, $this->dbuser, $this->dbpass, $this->dbname);
@@ -71,11 +86,38 @@ class Client
 
     public function update()
     {
-        return 'succesfully deleted user '. $this->firstname;
+        if (isset($this->firstname)) {
+            $this->updateQuery('firstname', $this->firstname);
+        }
+        if (isset($this->lastname)) {
+            $this->updateQuery('lastname', $this->lastname);
+        }
+        if (isset($this->email)) {
+            $this->updateQuery('email', $this->email);
+        }
+        if (isset($this->phonenumber1)) {
+            $this->updateQuery('phonenumber1', $this->phonenumber1);
+        }
+        if (isset($this->phonenumber2)) {
+            $this->updateQuery('phonenumber2', $this->phonenumber2);
+        }
+        if (isset($this->comment)) {
+            $this->updateQuery('comment', $this->comment);
+        }
+        return $query;
     }
 
     public function import()
     {
         return 'succesfully deleted user '. $this->firstname;
+    }
+
+    private function updateQuery($column, $field)
+    {
+        $mysqli = new mysqli($this->dbserver, $this->dbuser, $this->dbpass, $this->dbname);
+        $query = "UPDATE client SET $column = '$field' WHERE id = $this->id";
+        if (!$mysqli->query($query)) {
+            printf("Error: %s\n", $mysqli->error);
+        } 
     }
 }
