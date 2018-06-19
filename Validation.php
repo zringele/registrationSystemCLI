0cb985db0315267bcac27404f331b385d5d5e372
Zilvinas Ringele <zilvinas.ringele@gmail.com>
2018-06-19 13:22:35 +0300

    Update

--- Validation.php
+++ Validation.php
@@ -5,11 +5,17 @@ class Validation
 {
    public function validatePhoneNumber($number)
    {
-       if (substr($number, 0, 5) === '+3706' && strlen($number) === 12 && is_numeric($number) ){
-            return 'MobLT';
-       }
-       if (substr($number, 0, 2) === '86' && strlen($number) === 9 && is_numeric($number) ){
-            return 'MobLT';
-       }
+        if ( substr($number, 0, 4) === '+370' && strlen($number) === 12 && is_numeric($number) || ($number[0] === '86' && strlen($number) === 9 && is_numeric($number)) ){
+            return TRUE;
+        }
+        return FALSE;
+   }
+
+   public function validateEmail($email)
+   {
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            return FALSE;
+        }
+        return TRUE;
    }
 }
